c44fecae67afcb071b5fb16ad7c39f6091320929
EvtSasha <sasha.yevtushenko@aiesec.net>
2017-02-27 21:06:56 +0200

    Version1

---
+++ src/Tests/EditPlugins.java
@@ -0,0 +1,14 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Sasha on 27.02.2017.
+ */
+public class EditPlugins  extends BaseTest{
+
+    @Test
+    public void edithub() throws InterruptedException{
+
+    }
+}
